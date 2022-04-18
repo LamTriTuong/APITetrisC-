6e5b18189a3350abb478354ec97e4de18f109878
Your Name <you@example.com>
2022-04-18 15:29:57 +0700

    Tetris

---
+++ Tetris/Tetris/Model/ScoreModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Model
+{
+    internal class ScoreModel
+    {
+        public string NickName { get; set; }
+        public int Score { get; set; }
+        public DateTime LastTimePlay { get; set; }
+        public ScoreModel()
+        {
+
+        }
+    }
+}
